b35c5eef26f6f26097a1c61a18f19ba06946f3e3
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-03 01:18:52 +0300

    feat: implement orm-link-persistence module

---
+++ server/src/links/infrastructure/persistens/orm/orm-persistence.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { LinkRepository } from '@src/links/application/ports/links.repository'
+
+import { LinkEntity } from './entities/link.entities'
+import { TypeOrmLinkRepository } from './repositories/link.repository'
+
+@Module({
+    imports: [TypeOrmModule.forFeature([LinkEntity])],
+    providers: [
+        TypeOrmLinkRepository,
+        {
+            provide: LinkRepository,
+            useClass: TypeOrmLinkRepository,
+        },
+    ],
+    exports: [LinkRepository],
+})
+export class OrmLinkPersistenceModule {}
