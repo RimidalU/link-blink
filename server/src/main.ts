a8ad65d7b88da6a0edae7f6cdcc952c135897c94
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-02 10:44:24 +0300

    feat: add validation pipe to app

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        })
+    )
 
     const config = app.get(ConfigService)
 
