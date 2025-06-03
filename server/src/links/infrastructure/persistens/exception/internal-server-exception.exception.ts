467f52287ac07a45133c4088f04f4d88a1eb26c3
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-03 09:57:11 +0300

    feat: update links orm repository

--- server/src/links/infrastructure/persistens/exception/internal-server-exception.exception.ts
+++
@@ -1,7 +0,0 @@
-import { InternalServerErrorException } from '@nestjs/common'
-
-export class InternalServerException extends InternalServerErrorException {
-    constructor() {
-        super(`Internal Server Error`)
-    }
-}
--- server/src/links/infrastructure/persistens/exception/link-not-found.exception.ts
+++
@@ -1,7 +0,0 @@
-import { NotFoundException } from '@nestjs/common'
-
-export class LinkNotFoundException extends NotFoundException {
-    constructor(shortCode: string) {
-        super(`Link with shortcode '${shortCode}' not found.`)
-    }
-}
