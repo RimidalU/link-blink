0084257b1e7e9c941c573f3e417be4e05edc55e6
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-04 19:17:11 +0300

    feat: update get-link-info swaggr decoretor

--- server/src/links/presenters/http/decorators/get-link-info.swagger.decorator.ts
+++ server/src/links/presenters/http/decorators/get-link-info.swagger.decorator.ts
@@ -7,17 +7,19 @@ import {
 } from '@nestjs/swagger'
 
 import { LinkInfoDto } from '../dto/link-info.dto'
+import { LinkNotFoundDTO } from '../dto/link-note-found.response.dto'
+import { InternalServerErrorResponseDto } from '../dto/internal--server-error-response.dto'
 
 export function GetLinkInfoSwagger() {
     return applyDecorators(
         ApiOperation({ summary: 'Get link info' }),
         ApiNotFoundResponse({
             description: 'Not Found',
-            // type: LinkNotFoundDTO,
+            type: LinkNotFoundDTO,
         }),
         ApiInternalServerErrorResponse({
             description: 'Internal server error',
-            // type: InternalServerErrorResponseDto,
+            type: InternalServerErrorResponseDto,
         }),
         ApiResponse({
             status: 200,
