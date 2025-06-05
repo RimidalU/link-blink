2703c2350149cf88a97a1c16c627d4a09b995023
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-05 10:10:43 +0300

    feat: update delete-link swagger decorator

--- server/src/links/presenters/http/decorators/delete-link.swagger.decorator.ts
+++ server/src/links/presenters/http/decorators/delete-link.swagger.decorator.ts
@@ -6,21 +6,25 @@ import {
     ApiResponse,
 } from '@nestjs/swagger'
 
+import { LinkNotFoundDTO } from '../dto/link-note-found.response.dto'
+import { InternalServerErrorResponseDto } from '../dto/internal--server-error-response.dto'
+import { CreateLinkResponseDto } from '../dto/create-link-response.dto'
+
 export function DeleteLinkSwagger() {
     return applyDecorators(
         ApiOperation({ summary: 'Delete short link' }),
         ApiNotFoundResponse({
             description: 'Not Found',
-            // type: LinkNotFoundDTO,
+            type: LinkNotFoundDTO,
         }),
         ApiInternalServerErrorResponse({
             description: 'Internal server error',
-            // type: InternalServerErrorResponseDto,
+            type: InternalServerErrorResponseDto,
         }),
         ApiResponse({
             status: 204,
             description: 'Link created',
-            // type: SuccessResponseDto,
+            type: CreateLinkResponseDto,
         })
     )
 }
