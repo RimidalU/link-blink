217e40b97e2acd2a5dca1a2ee963609ee74e2f11
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-05 10:08:58 +0300

    feat: update get-link-analytics swagger decorator

--- server/src/links/presenters/http/decorators/get-link-analytics.swagger.decorator.ts
+++ server/src/links/presenters/http/decorators/get-link-analytics.swagger.decorator.ts
@@ -7,17 +7,19 @@ import {
 } from '@nestjs/swagger'
 
 import { AnalyticsDto } from '../dto/analytics.dto'
+import { LinkNotFoundDTO } from '../dto/link-note-found.response.dto'
+import { InternalServerErrorResponseDto } from '../dto/internal--server-error-response.dto'
 
 export function GetLinkAnalyticsSwagger() {
     return applyDecorators(
         ApiOperation({ summary: 'Get link analytics' }),
         ApiNotFoundResponse({
             description: 'Not Found',
-            // type: LinkNotFoundDTO,
+            type: LinkNotFoundDTO,
         }),
         ApiInternalServerErrorResponse({
             description: 'Internal server error',
-            // type: InternalServerErrorResponseDto,
+            type: InternalServerErrorResponseDto,
         }),
         ApiResponse({
             status: 200,
