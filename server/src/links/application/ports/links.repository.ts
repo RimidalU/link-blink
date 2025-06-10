905cb75ea4bedd8ed896d9df06d84bc691e52cfc
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-10 08:59:54 +0300

    feat: update abstract link-repository

--- server/src/links/application/ports/links.repository.ts
+++ server/src/links/application/ports/links.repository.ts
@@ -5,8 +5,8 @@ import { LinkInfoDto } from '@src/links/presenters/http/dto/link-info.dto'
 export abstract class LinkRepository {
     abstract create(link: Link): Promise<string>
     abstract findByAlias(alias: string): Promise<Link | null>
+    abstract findByLinkId(linkIdi: number): Promise<Link | null>
     abstract deleteByAlias(alias: string): Promise<string>
     abstract getInfo(alias: string): Promise<LinkInfoDto>
-    abstract getOriginalUrl(alias: string, ip: string): Promise<Link | null>
     abstract getAnalytics(alias: string): Promise<AnalyticsDto>
 }
