124e9a2e218551f8184aedbebfd638f690b92a93
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-10 09:00:20 +0300

    feat: update abstract link-click repository

--- server/src/links/application/ports/link-clicks.repository.ts
+++ server/src/links/application/ports/link-clicks.repository.ts
@@ -2,4 +2,9 @@ import { LinkClick } from '@src/links/domain/link-click'
 
 export abstract class LinkClicksRepository {
     abstract create(linkClick: LinkClick): Promise<void>
+    abstract countByLinkId(linkId: number): Promise<number>
+    abstract getLastIpsByLinkId(
+        linkId: number,
+        limit?: number
+    ): Promise<string[]>
 }
