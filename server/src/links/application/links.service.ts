cf643b5397f500b6487a5f065cf20a1f4cb3e0a0
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-14 00:56:12 +0300

    feat: implement redirect route cache

--- server/src/links/application/links.service.ts
+++ server/src/links/application/links.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { CacheRepository } from '@src/common/cache/ports/cache.repository'
 
 import { LinkInfoDto } from '../presenters/http/dto/link-info.dto'
 import { AnalyticsDto } from '../presenters/http/dto/analytics.dto'
 import { LinkFactory } from '../domain/factories/link-factory'
 import { LinkClickFactory } from '../domain/factories/link-click-factory'
+import { Link } from '../domain/link'
+import { LinkCacheMapper } from '../infrastructure/persistence/mappers/link-cash.mapper'
 
 import { CreateLinkCommand } from './commands/create-link.command'
 import { LinkRepository } from './ports/links.repository'
@@ -16,13 +19,16 @@ import { DeleteLinkCommand } from './commands/delete-link.command'
 import { GetLinkAnalyticsCommand } from './commands/get-link-analytics.command'
 import { LinkClicksRepository } from './ports/link-clicks.repository'
 
+const REDIRECT_CACHE_TTL = 24 * 60 * 60 * 1000 //  24 hours
+
 @Injectable()
 export class LinksService {
     constructor(
         private readonly linkRepository: LinkRepository,
         private readonly linkClicksRepository: LinkClicksRepository,
         private readonly linkFactory: LinkFactory,
-        private readonly linkClickFactory: LinkClickFactory
+        private readonly linkClickFactory: LinkClickFactory,
+        private readonly cacheRepository: CacheRepository
     ) {}
     async createLink(createLinkDto: CreateLinkCommand): Promise<string> {
         let { alias } = createLinkDto
@@ -50,10 +56,25 @@ export class LinksService {
         getOriginalUrlDTO: GetOriginalUrlCommand
     ): Promise<string> {
         const { alias, ip } = getOriginalUrlDTO
-        const link = await this.linkRepository.findByAlias(alias)
 
-        if (!link) {
-            throw new LinkNotFoundException(alias)
+        const cacheKey = `shortUrl:${alias}`
+        let link: Link | null
+
+        const cachedLinkJson = await this.cacheRepository.get(cacheKey)
+
+        if (!cachedLinkJson) {
+            link = await this.linkRepository.findByAlias(alias)
+            if (!link) {
+                throw new LinkNotFoundException(alias)
+            }
+
+            await this.cacheRepository.set(
+                cacheKey,
+                LinkCacheMapper.toJSON(link),
+                REDIRECT_CACHE_TTL //TODO: refactor from link entity
+            )
+        } else {
+            link = LinkCacheMapper.fromJSON(cachedLinkJson)
         }
 
         const newLinkClick = this.linkClickFactory.create({
--- server/src/links/infrastructure/persistence/links-infrastructure.module.ts
+++ server/src/links/infrastructure/persistence/links-infrastructure.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
-import { DRIVER_TYPE } from '@src/common/interfaces/application-bootstrap-options'
+import { DB_DRIVER_TYPE } from '@src/common/interfaces/application-bootstrap-options'
 
 import { InMemoryLinkPersistenceModule } from './in-memory/in-memory-persistence.module'
 import { OrmLinkPersistenceModule } from './orm/orm-persistence.module'
 
 @Module({})
 export class LinksInfrastructureModule {
-    static use(driver: DRIVER_TYPE) {
+    static use(driver: DB_DRIVER_TYPE) {
         const presentersModule =
             driver === 'orm'
                 ? OrmLinkPersistenceModule
