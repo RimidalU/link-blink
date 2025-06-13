b5a47a09b85767060f730d9afe5d1de25f697c2d
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-14 00:50:16 +0300

    fix: check and update expires-at payload field

--- server/src/links/domain/factories/link-factory.ts
+++ server/src/links/domain/factories/link-factory.ts
@@ -8,9 +8,15 @@ interface CreateLinkParams {
 
 export class LinkFactory {
     create(params: CreateLinkParams): Link {
-        const { alias, originalUrl, expiresAt } = params
+        const { alias, originalUrl } = params
+        let { expiresAt } = params
+
         const id = -1
         const createdAt = new Date()
+
+        if (expiresAt === undefined || expiresAt <= createdAt) {
+            expiresAt = new Date(createdAt.getTime() + 24 * 60 * 60 * 1000)
+        }
 
         return new Link(id, alias, originalUrl, createdAt, expiresAt)
     }
