b45718d8a42ad332d35d3902185c7c02bd958c85
uladzimir <stankevichuuladimiru@gmail.com>
2025-06-26 17:20:09 +0300

    feat: update validations

--- server/src/links/presenters/http/dto/internal--server-error-response.dto.ts
+++ server/src/links/presenters/http/dto/internal--server-error-response.dto.ts
@@ -1,21 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class InternalServerErrorResponseDto {
     @ApiProperty({
         example: 'Internal Server Error',
         description: 'Internal Server Error',
     })
+    @IsString()
+    @IsNotEmpty()
     readonly message: string
 
     @ApiProperty({
         example: 'Internal Server Error',
         description: 'Internal Server Error',
     })
+    @IsString()
+    @IsNotEmpty()
     readonly error: string
 
     @ApiProperty({
         example: 500,
         description: '500',
     })
+    @IsNumber()
+    @IsNotEmpty()
     readonly statusCode: number
 }
--- server/src/links/presenters/http/dto/link-note-found.response.dto.ts
+++ server/src/links/presenters/http/dto/link-note-found.response.dto.ts
@@ -1,21 +1,28 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
 
 export class LinkNotFoundDTO {
     @ApiProperty({
         example: "Link with alias 'my-link' not found",
         description: 'Link not found',
     })
+    @IsString()
+    @IsNotEmpty()
     readonly message: string
 
     @ApiProperty({
         example: 'Not Found',
         description: 'Not Found',
     })
+    @IsString()
+    @IsNotEmpty()
     readonly error: string
 
     @ApiProperty({
         example: 404,
         description: '404',
     })
+    @IsNumber()
+    @IsNotEmpty()
     readonly statusCode: number
 }
